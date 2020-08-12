c817f438994697b46490122cf2de9be25ae6a9ee
zhh904208603 <qewan@foxmail.com>
2020-08-12 22:36:28 +0800

    Initial Commit

---
+++ challenge2-mora/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  challenge2-mora
+//
+//  Created by 张洪Heo on 2020/8/12.
+//  Copyright © 2020 张洪Hoo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
